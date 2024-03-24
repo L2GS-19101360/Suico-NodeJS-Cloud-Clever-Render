389f7ef8ae8842713b2c1b56257cc9e5bbff5ec3
Lorenz Gil Suico <125742061+L2GS-19101360@users.noreply.github.com>
2024-03-24 15:29:17 +0800

    Update person.models.js

--- src/models/person.models.js
+++ src/models/person.models.js
@@ -9,7 +9,7 @@ const Person = function(person){
 }
 
 Person.findAll = function(result) {
-    dbConn.query("SELECT * FROM person", function(err, res){
+    dbConn.query("SELECT * FROM Person", function(err, res){
         if (err){
             console.log("Error: ", err);
             result(null, err);
